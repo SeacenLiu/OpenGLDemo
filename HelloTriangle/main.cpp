b0a16bca5c14d00550126d54ced1d959226d2b81
SeacenLiu <1016667972@qq.com>
2019-12-06 18:44:21 +0800

    update REARME

---
+++ HelloTriangle/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloTriangle
+//
+//  Created by SeacenLiu on 2019/12/6.
+//  Copyright © 2019 SeacenLiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # OpenGLDemo
 OpenGL Study Demo
+
+## 环境配置
+![./imgs/EnvironmentConfiguration.png](环境配置)
+
+- GLFW
+- GLAD
