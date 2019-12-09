09b768bdff92de3d11cc9d47eeffc4c85e18394d
SeacenLiu <1016667972@qq.com>
2019-12-09 14:13:22 +0800

    练习：使用不同的VBO和VAO绘制两个三角形

--- TwoTriangle/main.cpp
+++ TwoTriangle/main.cpp
@@ -6,10 +6,127 @@
 //  Copyright © 2019 SeacenLiu. All rights reserved.
 //
 
+/**
+ * 创建相同的两个三角形，但对它们的数据使用不同的VAO和VBO
+ */
+
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shaderhelp.h"
+#include "glhelp.h"
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+float vertices1[] = {
+    -0.5f,  0.5f, 0.0f,
+    -1.0f, -0.5f, 0.0f,
+     0.0f, -0.5f, 0.0f,
+};
+
+float vertices2[] = {
+     0.5f,  0.5f, 0.0f,
+     1.0f, -0.5f, 0.0f,
+     0.0f, -0.5f, 0.0f,
+};
+
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // --------------- 初始化 GLFW ---------------
+    glfwInitialize();
+    // --------------- 创建窗口 ---------------
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH,
+                                          SCR_HEIGHT,
+                                          "Two Triangle",
+                                          NULL,
+                                          NULL);
+    if (window == NULL) {
+        std::cout << "创建窗口失败" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // --------------- 初始化 GLAD ---------------
+    if (!gladInitialize()) {
+        std::cout << "初始化 GLAD 拓展失败" << std::endl;
+        return -1;
+    }
+    // --------------- 构建着色器程序 ---------------
+    GLuint shaderProgram = CreateShaderProgram(vertexShaderSource,
+                                               fragmentShaderSource);
+    if (!shaderProgram) {
+        std::cout << "着色器程序创建数百" << std::endl;
+        return -1;
+    }
+    // --------------- 配置 VBO & VAO ---------------
+    // VBO1 & VAO1
+    GLuint VBO1;
+    glGenBuffers(1, &VBO1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    GLuint VAO1;
+    glGenVertexArrays(1, &VAO1);
+    glBindVertexArray(VAO1);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    // VBO2 & VAO2
+    GLuint VBO2;
+    glGenBuffers(1, &VBO2);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+    GLuint VAO2;
+    glGenVertexArrays(1, &VAO2);
+    glBindVertexArray(VAO2);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    // --------------- 渲染循环 ---------------
+    while (!glfwWindowShouldClose(window)) {
+        // 1. 处理用户输入
+        processInput(window);
+        // 2. 背景渲染颜色
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // 3. 绘制三角形
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO2);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // 4. 交换缓冲
+        glfwSwapBuffers(window);
+        // 5. 处理事件
+        glfwPollEvents();
+    }
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
