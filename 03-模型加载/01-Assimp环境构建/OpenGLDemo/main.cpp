fa808b06d07a53579a33d772e5501d9a23ec0d5d
SeacenLiu <1016667972@qq.com>
2020-01-25 17:30:11 +0800

    Assimp 空项目环境配置

---
+++ 03-模型加载/01-Assimp环境构建/OpenGLDemo/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  OpenGLDemo
+//
+//  Created by SeacenLiu on 2020/1/6.
+//  Copyright © 2020 SeacenLiu. All rights reserved.
+//
+
+/**
+ * Assimp 构建
+ * https://learnopengl-cn.github.io/03%20Model%20Loading/01%20Assimp/
+ * #include "3rdparty/camera.h" // #include "camera.h" 会与Assimp的冲突
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
